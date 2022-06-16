3de5fc0e2444188e1d6173abadc44fb5cdd460f2
andreszs <10298071+andreszs@users.noreply.github.com>
2022-06-16 10:26:35 -0300

    1.0.1

--- www/GoogleCodeScanner.js
+++ www/GoogleCodeScanner.js
@@ -7,6 +7,23 @@ var GoogleCodeScanner = {
 	},
 	getBarcodeConstant: function (successCallback, errorCallback, args) {
 		exec(successCallback, errorCallback, "GoogleCodeScanner", "getBarcodeConstant", [args]);
+	},
+	BarcodeFormat: {
+		FORMAT_UNKNOWN: -1,
+		FORMAT_ALL_FORMATS: 0,
+		FORMAT_CODE_128: 1,
+		FORMAT_CODE_39: 2,
+		FORMAT_CODE_93: 4,
+		FORMAT_CODABAR: 8,
+		FORMAT_DATA_MATRIX: 16,
+		FORMAT_EAN_13: 32,
+		FORMAT_EAN_8: 64,
+		FORMAT_ITF: 128,
+		FORMAT_QR_CODE: 256,
+		FORMAT_UPC_A: 512,
+		FORMAT_UPC_E: 1024,
+		FORMAT_PDF417: 2048,
+		FORMAT_AZTEC: 4096
 	}
 };
 
